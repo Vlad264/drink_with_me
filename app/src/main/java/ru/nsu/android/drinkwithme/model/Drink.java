a28b058784cd94c88c5c94202b33132f91690365
Vlad264 <vlad-498@yandex.ru>
2020-06-03 17:28:28 +0700

    Create data class for drink

---
+++ app/src/main/java/ru/nsu/android/drinkwithme/model/Drink.java
@@ -0,0 +1,42 @@
+package ru.nsu.android.drinkwithme.model;
+
+public class Drink {
+    private long id;
+    private String name;
+    private int percent;
+
+    public Drink(String name, int percent) {
+        this.name = name;
+        this.percent = percent;
+    }
+
+    public Drink(long id, String name, int percent) {
+        this.id = id;
+        this.name = name;
+        this.percent = percent;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPercent() {
+        return percent;
+    }
+
+    public void setPercent(int percent) {
+        this.percent = percent;
+    }
+}
