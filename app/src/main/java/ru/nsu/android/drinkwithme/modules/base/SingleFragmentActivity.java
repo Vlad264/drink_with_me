9f867ed97c80ce1b23df88aea855c2b9eae6bb10
Vlad264 <vlad-498@yandex.ru>
2020-06-03 19:19:56 +0700

    Create bisic single fragment activity

---
+++ app/src/main/java/ru/nsu/android/drinkwithme/modules/base/SingleFragmentActivity.java
@@ -0,0 +1,30 @@
+package ru.nsu.android.drinkwithme.modules.base;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import ru.nsu.android.drinkwithme.R;
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    protected abstract Fragment createFirstFragment();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_fragment);
+
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragment_container, createFirstFragment())
+                .commit();
+    }
+
+    protected void replaceFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
+    }
+}
