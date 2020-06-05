9859bce28f6c237b8e375a777cf895a5c7e4ac63
Vlad264 <vlad-498@yandex.ru>
2020-06-05 18:27:40 +0700

    Update database for history

--- app/src/main/java/ru/nsu/android/drinkwithme/modules/database/history/HistoryDBHandler.java
+++ app/src/main/java/ru/nsu/android/drinkwithme/modules/database/history/HistoryDBHandler.java
@@ -8,6 +8,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import ru.nsu.android.drinkwithme.model.DrinkLiter;
 
@@ -24,10 +25,17 @@ public class HistoryDBHandler extends SQLiteOpenHelper implements IHistoryDBHand
     private static final String GROUP_TABLE_NAME = "group_table";
     private static final String GROUP_KEY = "group_number";
 
+    private static final String STATE_TABLE_NAME = "state";
+    private static final String STATE_KEY_GROUP = "group_id";
+    private static final String STATE_KEY_STATE = "state_result";
+
     private static final String INSERT = "INSERT INTO " + TABLE_NAME + "(" + KEY_GROUP_ID + "," + KEY_NAME + "," + KEY_PERCENT + "," + KEY_LITER + ") VALUES(?, ?, ?, ?)";
     private static final String DELETE = "DELETE FROM " + TABLE_NAME + " WHERE " + KEY_GROUP_ID + "=?";
 
     private static final String SELECT_GROUP = "SELECT * FROM " + TABLE_NAME + " WHERE " + KEY_GROUP_ID + "=?";
+
+    private AtomicInteger countOpen = new AtomicInteger(0);
+    private SQLiteDatabase writableDatabase;
 
     public HistoryDBHandler(Context context) {
         super(context, TABLE_NAME, null, VERSION);
@@ -48,34 +56,38 @@ public class HistoryDBHandler extends SQLiteOpenHelper implements IHistoryDBHand
 
     @Override
     public void add(DrinkLiter drink) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        String groupId = Long.toString(getGroupId(db));
+        SQLiteDatabase db = openDataBase();
+        String groupId = Long.toString(getGroupId());
         db.execSQL(INSERT, new String[] {groupId, drink.getName(), Integer.toString(drink.getPercent()), Double.toString(drink.getLiter())});
+        closeDataBase();
     }
 
     @Override
     public boolean nextGroup() {
-        SQLiteDatabase db = this.getWritableDatabase();
-        long currentId = getGroupId(db);
+        SQLiteDatabase db = openDataBase();
+        long currentId = getGroupId();
         boolean flag = false;
-        if (getGroup(db, currentId).size() != 0) {
+        if (getGroup(currentId).size() != 0) {
             String UPDATE_GROUP = "UPDATE " + GROUP_TABLE_NAME + " SET " + GROUP_KEY + "=?";
             db.execSQL(UPDATE_GROUP, new String[] {Long.toString(currentId + 1)});
             flag = true;
         }
-        db.close();
+        closeDataBase();
         return flag;
     }
 
     @Override
-    public List<DrinkLiter> getGroup(long id) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        List<DrinkLiter> result = getGroup(db, id);
-        db.close();
-        return result;
+    public void setState(int state) {
+        SQLiteDatabase db = openDataBase();
+        String groupId = Long.toString(getGroupId());
+        db.execSQL("DELETE FROM " + STATE_TABLE_NAME + " WHERE " + STATE_KEY_GROUP + "=?", new String[] {groupId});
+        db.execSQL("INSERT INTO " + STATE_TABLE_NAME + "(" + STATE_KEY_GROUP + "," + STATE_KEY_STATE + ") VALUES(?, ?)", new String[] {groupId, Integer.toString(state)});
+        closeDataBase();
     }
 
-    private List<DrinkLiter> getGroup(SQLiteDatabase db, long id) {
+    @Override
+    public List<DrinkLiter> getGroup(long id) {
+        SQLiteDatabase db = openDataBase();
         List<DrinkLiter> result = new LinkedList<>();
         Cursor cursor = db.rawQuery(SELECT_GROUP, new String[] {Long.toString(id)});
         if (cursor.moveToNext()) {
@@ -87,43 +99,92 @@ public class HistoryDBHandler extends SQLiteOpenHelper implements IHistoryDBHand
             } while (cursor.moveToNext());
         }
         cursor.close();
+        closeDataBase();
+        return result;
+    }
+
+    @Override
+    public List<DrinkLiter> getLastGroup() {
+        SQLiteDatabase db = openDataBase();
+        long currentId = getGroupId();
+        List<DrinkLiter> result = getGroup(currentId);
+        closeDataBase();
         return result;
     }
 
     @Override
     public List<List<DrinkLiter>> getAll() {
         List<List<DrinkLiter>> result = new LinkedList<>();
-        SQLiteDatabase db = this.getReadableDatabase();
-        long currentId = getGroupId(db);
+        SQLiteDatabase db = openDataBase();
+        long currentId = getGroupId();
         for (long i = 0; i <= currentId; ++i) {
-            result.add(getGroup(db, i));
+            result.add(getGroup(i));
         }
-        db.close();
+        closeDataBase();
+        return result;
+    }
+
+    @Override
+    public int getState(long id) {
+        SQLiteDatabase db = openDataBase();
+        String SELECT_GROUP = "SELECT * FROM " + STATE_TABLE_NAME + " WHERE " + STATE_KEY_GROUP + "=?";
+        Cursor cursor = db.rawQuery(SELECT_GROUP, new String[] {Long.toString(id)});
+        if (cursor.moveToFirst()) {
+            int state = cursor.getInt(1);
+            cursor.close();
+            closeDataBase();
+            return state;
+        }
+        cursor.close();
+        closeDataBase();
+        return -1;
+    }
+
+    @Override
+    public int getLastState() {
+        long currentId = getGroupId();
+        return getState(currentId);
+    }
+
+    @Override
+    public List<Integer> getAllStates() {
+        List<Integer> result = new LinkedList<>();
+        SQLiteDatabase db = openDataBase();
+        long currentId = getGroupId();
+        for (long i = 0; i <= currentId; ++i) {
+            result.add(getState(i));
+        }
+        closeDataBase();
         return result;
     }
 
     @Override
     public void deleteGroup(long id) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        String groupId = Long.toString(getGroupId(db));
+        SQLiteDatabase db = openDataBase();
+        String groupId = Long.toString(getGroupId());
         db.execSQL(DELETE, new String[] {groupId});
-        db.close();
+        closeDataBase();
     }
 
     @Override
     public void deleteAll() {
-        SQLiteDatabase db = this.getWritableDatabase();
+        SQLiteDatabase db = openDataBase();
         onUpgrade(db, 0, 0);
-        db.close();
+        closeDataBase();
     }
 
-    private long getGroupId(SQLiteDatabase db) {
+    private long getGroupId() {
+        SQLiteDatabase db = openDataBase();
         String SELECT_GROUP = "SELECT * FROM " + GROUP_TABLE_NAME;
         Cursor cursor = db.rawQuery(SELECT_GROUP, null);
         if (cursor.moveToFirst()) {
             long id = cursor.getLong(0);
+            cursor.close();
+            closeDataBase();
             return id;
         }
+        cursor.close();
+        closeDataBase();
         return 0;
     }
 
@@ -131,6 +192,7 @@ public class HistoryDBHandler extends SQLiteOpenHelper implements IHistoryDBHand
         try {
             db.execSQL("SELECT * FROM " + TABLE_NAME);
             db.execSQL("SELECT * FROM " + GROUP_TABLE_NAME);
+            db.execSQL("SELECT * FROM " + STATE_TABLE_NAME);
             return true;
         } catch (SQLException e) {
             return false;
@@ -145,10 +207,31 @@ public class HistoryDBHandler extends SQLiteOpenHelper implements IHistoryDBHand
                 + KEY_PERCENT + " INTEGER,"
                 + KEY_LITER + " REAL" + ")";
         db.execSQL(CREATE_TABLE);
+
         String CREATE_GROUP_TABLE = "CREATE TABLE " + GROUP_TABLE_NAME + "("
                 + GROUP_KEY + " INTEGER" + ")";
         db.execSQL(CREATE_GROUP_TABLE);
         String INSET_FIRST_GROUP = "INSERT INTO " + GROUP_TABLE_NAME + "(" + GROUP_KEY + ") VALUES(?)";
         db.execSQL(INSET_FIRST_GROUP, new String[] {Long.toString(0)});
+
+        String CREATE_STATE_TABLE = "CREATE TABLE " + STATE_TABLE_NAME + "("
+                + STATE_KEY_GROUP + " INTEGER PRIMARY KEY,"
+                + STATE_KEY_STATE + " INTEGER" + ")";
+        db.execSQL(CREATE_STATE_TABLE);
+    }
+
+    private SQLiteDatabase openDataBase() {
+        synchronized (this) {
+            if (countOpen.incrementAndGet() == 1) {
+                writableDatabase = getWritableDatabase();
+            }
+        }
+        return writableDatabase;
+    }
+
+    private void closeDataBase() {
+        if (countOpen.decrementAndGet() == 0) {
+            writableDatabase.close();
+        }
     }
 }
--- app/src/main/java/ru/nsu/android/drinkwithme/modules/database/history/IHistoryDBHandler.java
+++ app/src/main/java/ru/nsu/android/drinkwithme/modules/database/history/IHistoryDBHandler.java
@@ -7,8 +7,13 @@ import ru.nsu.android.drinkwithme.model.DrinkLiter;
 public interface IHistoryDBHandler {
     void add(DrinkLiter drink);
     boolean nextGroup();
+    void setState(int state);
     List<DrinkLiter> getGroup(long id);
+    List<DrinkLiter> getLastGroup();
     List<List<DrinkLiter>> getAll();
+    int getState(long id);
+    int getLastState();
+    List<Integer> getAllStates();
     void deleteGroup(long id);
     void deleteAll();
 }
